044c74bdbd6ecbb2dc7695909e49805a11fa56af
Abhishek <abhishekvirat0@gmail.com>
2021-08-13 11:42:05 +0530

    static files added

--- icds/settings.py
+++ icds/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'foodIcds/static'),
 )
 
 #  Add configuration for static files storage using whitenoise
